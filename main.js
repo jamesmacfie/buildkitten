4cf15ee4997fa4fafa3c3627943f7db76cb23333
James Macfie <james@macfie.co.nz>
2017-02-02 16:12:47 +1300

    Add in context menu

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const menubar = require('menubar')
+const { app, Menu } = require('electron');
 
 const mb = menubar({
   index: 'https://buildkite.com/builds',
@@ -21,6 +22,55 @@ function loadCSS() {
   });
 }
 
+function setUpMainMenu() {
+  const template = [{
+    label: "My BuildKite",
+    submenu: [
+      { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+  ]}, {
+    label: "Edit",
+    submenu: [
+      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+      { type: "separator" },
+      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]
+  }, {
+    label: 'Help',
+    submenu: [{
+      label: 'Raise an issue',
+      click() {
+        shell.openExternal('https://github.com/jamesmacfie/buildkite/issues');
+      }
+    }]
+}];
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+}
+
+function setupContextMenu() {
+  mb.window.webContents.on('context-menu', (e, props) => {
+    const {
+      x,
+      y
+    } = props;
+
+    Menu.buildFromTemplate([{
+      label: 'Inspect element',
+      click() {
+        mb.window.inspectElement(x, y);
+      }
+    }]).popup(mb.window);
+  });
+}
+
+
 mb.on('ready', () => {
+  setUpMainMenu();
+  setupContextMenu();
   mb.window.webContents.on('did-navigate', loadCSS);
 })
